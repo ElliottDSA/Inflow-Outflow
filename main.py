96645f8e04163c51d145b25c7ce4e8fe280f0029
masked_avenger <elliott.lapinel@mwvcaa.org>
2023-06-02 10:28:03 -0700

    adding a list of people housed in the month

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
         short_pdr = short_pdr.drop_duplicates(subset = ["Client Id"])
 
         #Add housing move in date code ******************
-
+        housed_pdr = short_pdr[short_pdr['HMID']]
         #End housing move in date code *************************
 
         #Get most recent row of bfz_frame!
